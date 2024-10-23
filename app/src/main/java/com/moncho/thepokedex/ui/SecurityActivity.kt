a7a5efff4eee3aa55438a03c782f32537dabfa5f
Ramon Delgado <rdelgadou@gmail.com>
2024-10-22 20:14:23 -0700

    - Login/Sign Up screen added

--- app/src/main/java/com/moncho/thepokedex/ui/SecurityActivity.kt
+++ app/src/main/java/com/moncho/thepokedex/ui/SecurityActivity.kt
@@ -1,21 +1,100 @@
 package com.moncho.thepokedex.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.moncho.thepokedex.MainActivity
 import com.moncho.thepokedex.R
+import com.moncho.thepokedex.databinding.ActivityMainBinding
+import com.moncho.thepokedex.databinding.ActivitySecurityBinding
 
 class SecurityActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecurityBinding
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_security)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySecurityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.showLoginButton.setOnClickListener {
+            binding.loginLayout.visibility = View.VISIBLE
+            binding.singupLayout.visibility = View.GONE
         }
+
+        binding.showSingUpButton.setOnClickListener {
+            binding.loginLayout.visibility = View.GONE
+            binding.singupLayout.visibility = View.VISIBLE
+        }
+
+        binding.singupButton.setOnClickListener {
+            val email = binding.signUpEmailEditText.text.toString()
+            val password = binding.singUpPasswordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        showMain()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        showMain()
+                    }
+                }
+        }
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        showMain()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                       showMain()
+                    }
+                }
+        }
+    }
+
+    fun showMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    companion object {
+        const val TAG = "SecurityActivity"
     }
 }
